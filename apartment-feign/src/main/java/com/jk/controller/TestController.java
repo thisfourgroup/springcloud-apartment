f16dc2152847d4c1564722fd09654ab6d41530ef
zhaoziyao123 <425031808@qq.com>
2019-04-16 11:58:03 +0800

    init project

---
+++ apartment-feign/src/main/java/com/jk/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.jk.controller;
+
+import com.jk.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @Autowired
+    TestService testService;
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String testF(){
+
+       return testService.testF();
+    }
+}
